b6634ce73bf62942208c885948d6e7fde40ead69
daviddgamble <daviddgamble@gmail.com>
2022-11-08 14:11:37 -0800

    Add Player() and Player.prototype.turnScore to pigdice.js and tests. All tested and passed.
    Co-Authored-by: teddy atkinson <tedddy.atkinson@gmail.com>
    co-authored-by: chris loveless <chriskloveless@gmail.com>

--- __tests__/pigdice.test.js
+++ __tests__/pigdice.test.js
@@ -1,10 +1,35 @@
-import {roll} from  "../src/pigdice.js";
+import { roll, Player } from "../src/pigdice.js";
 
 describe("roll", () => {
 
   test('should return a random number', () => {
     let randomNum = roll();
     expect(randomNum).toBeGreaterThanOrEqual(1);
-    expect(randomNum).toBeLessThan(6);  });
+    expect(randomNum).toBeLessThanOrEqual(6);
+  });
 });
+
+describe("Player", () => {
+
+  test('should create a player object with score, turn, and currentScore properties', () => {
+    let newPlayer = new Player(0, true, 0);
+    expect(newPlayer.score).toEqual(0);
+    expect(newPlayer.turn).toEqual(true);
+    expect(newPlayer.currentScore).toEqual(0);
+  });
+
+  test('Should update current score to 0 after a roll of 1.', () => {
+    let player1 = new Player(0, true, 4);
+    player1.turnScore(1);
+    expect(player1.currentScore).toEqual(0);
+  });
+
+  test('Should update current score to 5 after a roll of 5.', () => {
+    let player1 = new Player(0, true, 0);
+    player1.turnScore(5);
+    expect(player1.currentScore).toEqual(5);
+  });
+});
+
+
 
--- src/pigdice.js
+++ src/pigdice.js
@@ -2,3 +2,17 @@ export function roll() {
   let randNum = Math.floor(Math.random() * 6) + 1;
   return randNum;
 }
+
+export function Player(score, turn, currentScore) {
+  this.score = score;
+  this.turn = turn;
+  this.currentScore = currentScore;
+}
+
+Player.prototype.turnScore = function(roll) {
+  if (roll === 1) {
+    this.currentScore = 0;
+  } else {
+    this.currentScore += roll;
+  }
+}
